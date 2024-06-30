9b3951c1a99731d6486999222eecff1db791fc83
laksh2444 <lakshpaul1512@gmail.com>
2024-06-30 14:29:17 +0530

    Updated repository with changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import './App.css';
 import Body from './Body';
-import Contact_us from './Contact_us';
-import First_part from './First_part';
-import Head from './Head';
+import FAQ from './FAQ';
+import Header from './Header';
+
 
 
   function App() {
   return (
     <div className="App">
-      <Head></Head>
-      <First_part></First_part>
+      <Header></Header>
       <Body></Body>
-      <Contact_us></Contact_us>
+      <FAQ></FAQ>
     </div>
   );
 }
